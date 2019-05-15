596f2f6e00b92903c149208ca601e4e509bfdef9
Oliver Liang <oliver@engturtle.com>
2019-05-14 22:32:09 -0400

    changed graph creation to be focused on edges, still need to add back traversal when a new vertex is created

--- VISSIM_helpers.py
+++ VISSIM_helpers.py
@@ -8,6 +8,7 @@ class VissimRoadNet(igraph.Graph):
     This class extends the igraph Graph object to allow connection with a VISSIM
     """
     VISSIM_Edge_Attributes = ['No', 'FromNode', 'ToNode', 'FromEdges', 'ToEdges', 'IsTurn', 'Type', 'Closed']
+
     def __init__(self, net, *args, **kwargs):
         """
         Initializes the VissimRoadNet graph using a VISSIM network
@@ -17,7 +18,7 @@ class VissimRoadNet(igraph.Graph):
         """
         super(VissimRoadNet, self).__init__(directed=True, *args, **kwargs)
         self.net = net
-        self.vissim_net_to_igraph(net)
+        self.visedges = self.vissim_net_to_igraph(self.net)
 
     def vissim_net_to_igraph(self, vissim_net):
         """
@@ -32,10 +33,12 @@ class VissimRoadNet(igraph.Graph):
             [list(row) for row in vissim_net.Edges.GetMultipleAttributes(self.VISSIM_Edge_Attributes)],
             columns=self.VISSIM_Edge_Attributes
         )
+        # filter only open dynamic assignment edges
+        all_edges = all_edges[all_edges.Type == 'DYNAMICASSIGNMENT']
+        # properly type cast all the columns
         all_edges.No = all_edges.No.astype('int32')
         all_edges.FromNode = all_edges.FromNode.astype('int32')
         all_edges.ToNode = all_edges.ToNode.astype('int32')
-        all_edges.set_index('No', inplace=True, verify_integrity=True, drop=False)
         all_edges.astype({
             'FromEdges': str,
             'ToEdges': str,
@@ -45,10 +48,44 @@ class VissimRoadNet(igraph.Graph):
         }, copy=False)
         all_edges['OriginVertex'] = ""
         all_edges['DestinVertex'] = ""
+        # use edge numbers as index
+        all_edges.set_index('No', inplace=True, verify_integrity=True, drop=False)
 
         # read each edge into graph, build vertices as we go
-        for index, edge in all_edges.iterrows():
-            pass
+        for index in all_edges.index:
+            if not all_edges.loc[index].OriginVertex:  # fresh edge that doesn't have origin vertices assigned
+                # create and add origin vertex to graph
+                vertex_name = 'visnode.' + str(all_edges.loc[index].FromNode) + '.' + str(index)
+                self.add_vertex(name=vertex_name)
+                all_edges.loc[index, 'OriginVertex'] = vertex_name
+
+            # add this edge's origin vertex as DestinVertex of all edges going into it
+            from_edges = all_edges.loc[index].FromEdges
+            if from_edges:
+                from_edges = [int(ed) for ed in from_edges.split(',')]
+                all_edges.loc[from_edges, 'DestinVertex'] = all_edges.loc[index, 'OriginVertex']
+                # TODO add the vertex to the OriginVertex of other edges going to the same edges
+
+            if not all_edges.loc[index].DestinVertex:  # fresh edge that doesn't have destination vertices assigned
+                # create and add destination vertex to graph
+                vertex_name = 'visnode.' + str(all_edges.loc[index].ToNode) + '.' + str(index)
+                self.add_vertex(name=vertex_name)
+                all_edges.loc[index, 'DestinVertex'] = vertex_name
+
+            # add this edge's destination vertex as OriginVertex of all edges going from it
+            to_edges = all_edges.loc[index].ToEdges
+            if to_edges:
+                to_edges = [int(ed) for ed in to_edges.split(',')]
+                all_edges.loc[to_edges, 'OriginVertex'] = all_edges.loc[index, 'DestinVertex']
+                # TODO add the vertex to the DestinVertex of other edges coming from the same edges
+
+            # add this edge to the graph
+            if not all_edges.loc[index, 'Closed']:
+                self.add_edge(all_edges.loc[index, 'OriginVertex'],
+                              all_edges.loc[index, 'DestinVertex'],
+                              name="visedge." + str(index))
+
+        return all_edges
 
 
 # Testing code
@@ -58,7 +95,6 @@ if __name__ == "__main__":
 
     FileName = r"C:\Users\Public\Documents\PTV Vision\PTV Vissim 9\Examples Demo\Urban Freeway Dyn Assign Redmond.US\I405 OD"
     Vissim.LoadNet(FileName + ".inpx")
-    #Vissim.LoadLayout(FileName + ".layx")
     Net = Vissim.Net
     if not Net.DynamicAssignment.CreateGraph(c.CGEdgeTypeDynamicAssignment):
         print("VISSIM Network Graph Creation error")
@@ -66,3 +102,4 @@ if __name__ == "__main__":
 
     road_graph = VissimRoadNet(Net)
     road_graph.write_svg(r"E:\Thesis\test.svg")
+    Vissim.Exit()
