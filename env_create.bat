ec047ed8f132c06c5bb49bc0badc5f32fbd07c99
Oliver Liang <oliver@engturtle.com>
2019-05-15 21:07:16 -0400

    changed environment creation script

--- env_create.bat
+++ env_create.bat
@@ -1,4 +1,4 @@
-conda create -n oliver python=3.6 scipy pywin32 pip jupyter jupyterlab qgrid -c conda-forge ipywidgets
+conda create -n oliver python=3.6 scipy pywin32 pip jupyter jupyterlab qgrid ipywidgets -c conda-forge 
 conda activate oliver
 pip install https://download.lfd.uci.edu/pythonlibs/q5gtlas7/python_igraph-0.7.1.post6-cp36-cp36m-win_amd64.whl
 pip install jupyterlab_code_formatter jupyterlab-git
