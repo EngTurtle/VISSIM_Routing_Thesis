7192db7ba66fb0cd9ba2570d55e17af4fea68928
Oliver Liang <oliver@engturtle.com>
2019-06-11 15:09:18 -0400

    updated environment files, and added interval filtering to plot

--- env_create.sh
+++ env_create.sh
@@ -6,7 +6,7 @@ conda activate thesis
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter nbextension enable --py --sys-prefix qgrid
 
-jupyter labextension install @jupyter-widgets/jupyterlab-manager @ryantam626/jupyterlab_code_formatter @jupyterlab/git qgrid @jupyterlab/plotly-extension
+jupyter labextension install @jupyter-widgets/jupyterlab-manager @ryantam626/jupyterlab_code_formatter @jupyterlab/git qgrid @jupyterlab/plotly-extension @krassowski/jupyterlab_go_to_definition
 
 jupyter serverextension enable --py jupyterlab_code_formatter
 jupyter serverextension enable --py jupyterlab_git
